16812e2dcb01fc0a98d7cd0380cdc9cbf95bed6d
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2024-02-16 13:48:21 -0500

    Update ARM docker images.

--- linux_arm32_build/build_cmake.sh
+++ linux_arm32_build/build_cmake.sh
@@ -2,9 +2,9 @@
 set -e
 mkdir /cmake
 cd cmake
-curl -L https://github.com/Kitware/CMake/releases/download/v3.19.5/cmake-3.19.5.tar.gz >> cmake.tar.gz
+curl -L https://github.com/Kitware/CMake/releases/download/v3.28.3/cmake-3.28.3.tar.gz >> cmake.tar.gz
 tar -xvf cmake.tar.gz > /dev/null
-cd cmake-3.19.5
+cd cmake-3.28.3
 export CFLAGS="-D_FILE_OFFSET_BITS=64"
 export CXXFLAGS="-D_FILE_OFFSET_BITS=64"
 ./bootstrap --parallel=8
